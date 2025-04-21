cfe7b39e8706ee3dd5dc639cd664f75f0e634ca5
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-04-21 17:24:45 -0500

    feat: :lipstick: Se agregaron las opciones de letras

--- js/main.js
+++ js/main.js
@@ -2,5 +2,33 @@ let start = document.getElementById('title__div')
 
 start.innerHTML = `
 <h1 class="welcome">Bienvenido, prueba tu suerte</h1>
-
+<ul class= "list-abc">
+<button>a</button>
+<button>b</button>
+<button>c</button>
+<button>d</button>
+<button>f</button>
+<button>g</button>
+<button>h</button>
+<button>i</button>
+<button>j</button>
+<button>k</button>
+<button>l</button>
+<button>m</button>
+<button>n</button>
+<button>ñ</button>
+<button>o</button>
+<button>p</button>
+<button>q</button>
+<button>r</button>
+<button>s</button>
+<button>t</button>
+<button>u</button>
+<button>v</button>
+<button>w</button>
+<button>x</button>
+<button>y</button>
+<button>z</button>
+<button></button>
+</ul>
 `
